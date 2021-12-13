e8323ee9afbc903909f7552399fea6454fe7ba14
Sam C <sc14809@my.bristol.ac.uk>
2021-12-13 21:41:10 +0000

    Template params for ParserDef

--- examples/simple.rs
+++ examples/simple.rs
@@ -6,11 +6,11 @@ use sylo::ast::*;
 use regex_deriv as re;
 
 fn main() {
-    let mut def = ParserDef::default();
-    def.tokens.push(Token { name: Ident("DIGIT".into()), regex: re::RegEx::range32('0' as u32, '9' as u32) });
+    let mut def: ParserDef<_, ()> = ParserDef::new();
+    def.tokens.push(Token { name: "DIGIT".to_string(), regex: re::RegEx::range32('0' as u32, '9' as u32) });
     def.rules.push(Rule {
-        name: Ident("Expr".into()),
-        expr: Expr::Opt(Box::new(Expr::Token(Ident("DIGIT".into())))),
+        name: "Expr".to_string(),
+        expr: Expr::Opt(Box::new(Expr::Token("DIGIT".to_string()))),
     });
     let (lexicon, grammar) = def.compile();
     println!("{:?}", grammar.productions().into_iter().collect::<Vec<_>>())
--- src/ast.rs
+++ src/ast.rs
@@ -2,57 +2,63 @@ use std::collections::{HashSet, HashMap, hash_map::Entry::{Occupied, Vacant}};
 use crate::re::{self, RegEx};
 use crate::lr::grammar::{Grammar, GrammarBuilder, Symbol, Symbol::Terminal as Word, Symbol::Variable as Var};
 
-#[derive(Default)]
-pub struct ParserDef{
-    pub tokens: Vec<Token>,
-    pub rules: Vec<Rule>,
+pub struct ParserDef<Ident, Code> {
+    pub tokens: Vec<Token<Ident>>,
+    pub rules: Vec<Rule<Ident, Code>>,
 }
 
-#[derive(Clone, PartialEq, Eq, Hash)]
-pub struct Ident(pub String);
-
 #[derive(Clone)]
-pub enum ActionCode {
+pub enum Action<Code> {
     Forward,
-    Seq(String),
+    Seq(Code),
     None,
     Option,
     Vec,
 }
 
-pub struct Token {
+pub struct Token<Ident> {
     pub name: Ident,
     pub regex: RegEx,
 }
 
-pub struct Rule {
+pub struct Rule<Ident, Code> {
     pub name: Ident,
-    pub expr: Expr,
+    pub expr: Expr<Ident, Code>,
 }
 
-pub struct NamedExpr {
+pub struct NamedExpr<Ident, Code> {
     pub name: Option<Ident>,
-    pub expr: Expr,
+    pub expr: Expr<Ident, Code>,
 }
 
-pub enum Expr {
+pub enum Expr<Ident, Code> {
     Literal(String),
     Token(Ident),
     Rule(Ident),
-    Seq(Vec<NamedExpr>, String),
-    Alt(Vec<Expr>),
-    Opt(Box<Expr>),
-    Star(Box<Expr>),
-    Plus(Box<Expr>),
+    Seq(Vec<NamedExpr<Ident, Code>>, Code),
+    Alt(Vec<Expr<Ident, Code>>),
+    Opt(Box<Expr<Ident, Code>>),
+    Star(Box<Expr<Ident, Code>>),
+    Plus(Box<Expr<Ident, Code>>),
 }
 
-impl ParserDef {
+impl<Ident, Code> ParserDef<Ident, Code>
+where
+    Ident: Clone + Eq + std::hash::Hash,
+    Code: Clone,
+{
+    #[must_use]
+    #[allow(clippy::new_without_default)]
+    pub fn new() -> Self {
+        Self { tokens: Vec::new(), rules: Vec::new() }
+    }
+
     /// # Panics
     #[must_use]
     pub fn compile(&self) -> (Vec<RegEx>, Grammar) {
         let translator = Translator::new(self);
         
-        let lexicon = translator.literals.into_iter().map(|literal| crate::re::literal(&literal))
+        let lexicon = translator.literals.keys().map(|literal| crate::re::literal(&literal))
             .chain(self.tokens.iter().map(|token| token.regex.clone()))
             .collect();
 
@@ -68,59 +74,57 @@ impl ParserDef {
 // === INTERNALS ===
 // =================
 
-struct Translator {
-    literals: HashSet<String>,
-    litmap: HashMap<String, Symbol>,
-    tokenmap: HashMap<Ident, Symbol>,
-    varmap: HashMap<Ident, Symbol>,
-    rules: Vec<Vec<Production>>,
+struct Translator<Ident, Code> {
+    literals: HashMap<String, Symbol>,
+    tokens: HashMap<Ident, Symbol>,
+    variables: HashMap<Ident, Symbol>,
+    rules: Vec<Vec<Production<Code>>>,
 }
 
 #[derive(Clone)]
-struct Production {
+struct Production<Code> {
     symbols: Vec<Symbol>,
-    action: ActionCode,
+    action: Action<Code>,
 }
 
-enum Ret {
+enum Ret<Code> {
     Symbol(Symbol),
-    Production(Production),
-    Alt(Vec<Production>),
+    Production(Production<Code>),
+    Alt(Vec<Production<Code>>),
 }
 
-impl Translator {
-    fn new(def: &ParserDef) -> Self {
-        let literals: HashSet<String> = {
-            let mut literals = HashSet::new();
-            for rule in &def.rules {
-                collect_literals(&rule.expr, &mut literals);
-            }
+impl<Ident, Code> Translator<Ident, Code>
+where
+    Ident: Clone + Eq + std::hash::Hash,
+    Code: Clone,
+{
+    fn new(def: &ParserDef<Ident, Code>) -> Self {
+        let literals = {
+            let mut literals = HashMap::new();
+            def.rules.iter().fold(0, |acc, rule| collect_literals(&rule.expr, &mut literals, acc));
             literals
         };
 
-        let litmap: HashMap<String, Symbol> = literals.iter().enumerate()
-            .map(|(i, literal)| (literal.clone(), Word(i))).collect();
-
-        let tokenmap: HashMap<Ident, Symbol> = def.tokens.iter().enumerate()            
-            .map(|(i, token)| (token.name.clone(), Word(i + litmap.len()))).collect();
+        let tokens: HashMap<Ident, Symbol> = def.tokens.iter().enumerate()            
+            .map(|(i, token)| (token.name.clone(), Word(i + literals.len()))).collect();
         
-        let varmap: HashMap<Ident, Symbol> = {
-            let mut varmap = HashMap::new();
-            let mut varcount = 0;
+        let variables: HashMap<Ident, Symbol> = {
+            let mut variables = HashMap::new();
+            let mut count = 0;
             for rule in &def.rules {
-                match varmap.entry(rule.name.clone()) {
+                match variables.entry(rule.name.clone()) {
                     Occupied(_) => panic!("Rule with that name already exists."),
-                    Vacant(entry) => { entry.insert(Var(varcount)); varcount += 1; }
+                    Vacant(entry) => { entry.insert(Var(count)); count += 1; }
                 }
             }
-            varmap
+            variables
         };
 
-        let mut translator = Translator { literals, litmap, tokenmap, varmap, rules: vec![Vec::new(); def.rules.len()] };
+        let mut translator = Translator { literals, tokens, variables, rules: vec![Vec::new(); def.rules.len()] };
 
         for i in 0..def.rules.len() {
             translator.rules[i] = match translator.visit(&def.rules[i].expr) {
-                Ret::Symbol(symbol) => vec![Production { symbols: vec![symbol], action: ActionCode::Forward }],
+                Ret::Symbol(symbol) => vec![Production { symbols: vec![symbol], action: Action::Forward }],
                 Ret::Production(production) => vec![production],
                 Ret::Alt(productions) => productions,
             }
@@ -129,13 +133,13 @@ impl Translator {
         translator
     }
 
-    fn add_rule(&mut self, productions: Vec<Production>) -> usize {
+    fn add_rule(&mut self, productions: Vec<Production<Code>>) -> usize {
         let index = self.rules.len();
         self.rules.push(productions);
         index
     }
 
-    fn get_symbol(&mut self, expr: &Expr) -> Symbol {
+    fn get_symbol(&mut self, expr: &Expr<Ident, Code>) -> Symbol {
         match self.visit(expr) {
             Ret::Symbol(symbol) => symbol,
             Ret::Production(production) => Var(self.add_rule(vec![production])),
@@ -143,26 +147,26 @@ impl Translator {
         }
     }
 
-    fn visit(&mut self, parent: &Expr) -> Ret {
+    fn visit(&mut self, parent: &Expr<Ident, Code>) -> Ret<Code> {
         match parent {
             Expr::Literal(literal) => {
-                Ret::Symbol(self.litmap[literal])
+                Ret::Symbol(self.literals[literal])
             },
             Expr::Token(ident) => {
-                Ret::Symbol(self.tokenmap[ident])
+                Ret::Symbol(self.tokens[ident])
             },
             Expr::Rule(ident) => {
-                Ret::Symbol(self.varmap[ident])
+                Ret::Symbol(self.variables[ident])
             },
             Expr::Seq(named_exprs, code) => {
                 let symbols = named_exprs.iter().map(|named_expr| self.get_symbol(&named_expr.expr)).collect();
-                Ret::Production(Production { symbols, action: ActionCode::Seq(code.clone()) })
+                Ret::Production(Production { symbols, action: Action::Seq(code.clone()) })
             },
             Expr::Alt(exprs) => {
                 let mut new_productions = Vec::new();
                 for expr in exprs {
                     match self.visit(expr) {
-                        Ret::Symbol(symbol) => new_productions.push(Production { symbols: vec![symbol], action: ActionCode::Forward }),
+                        Ret::Symbol(symbol) => new_productions.push(Production { symbols: vec![symbol], action: Action::Forward }),
                         Ret::Production(production) => new_productions.push(production),
                         Ret::Alt(productions) => new_productions.extend(productions),
                     }
@@ -171,37 +175,37 @@ impl Translator {
             },
             Expr::Opt(expr) => {
                 Ret::Alt(vec![
-                    Production { symbols: vec![self.get_symbol(expr)], action: ActionCode::Option },
-                    Production { symbols: Vec::new(), action: ActionCode::None },
+                    Production { symbols: vec![self.get_symbol(expr)], action: Action::Option },
+                    Production { symbols: Vec::new(), action: Action::None },
                 ])
             },
             Expr::Star(expr) => {
                 let index = self.add_rule(Vec::new());
                 let symbol = self.get_symbol(expr);
-                self.rules[index].push(Production { symbols: vec![Var(index), symbol], action: ActionCode::Vec });
-                self.rules[index].push(Production { symbols: Vec::new(), action: ActionCode::None });
+                self.rules[index].push(Production { symbols: vec![Var(index), symbol], action: Action::Vec });
+                self.rules[index].push(Production { symbols: Vec::new(), action: Action::None });
                 Ret::Symbol(Var(index))
             },
             Expr::Plus(expr) => {
                 let index = self.add_rule(Vec::new());
                 let symbol = self.get_symbol(expr);
-                self.rules[index].push(Production { symbols: vec![Var(index), symbol], action: ActionCode::Vec });
-                self.rules[index].push(Production { symbols: vec![symbol], action: ActionCode::Forward });
+                self.rules[index].push(Production { symbols: vec![Var(index), symbol], action: Action::Vec });
+                self.rules[index].push(Production { symbols: vec![symbol], action: Action::Forward });
                 Ret::Symbol(Var(index))
             },
         }
     }
 }
 
-fn collect_literals(parent: &Expr, literals: &mut HashSet<String>) {
+fn collect_literals<Ident, Code>(parent: &Expr<Ident, Code>, literals: &mut HashMap<String, Symbol>, count: usize) -> usize {
     match parent {
-        Expr::Literal(literal)    => { literals.insert(literal.clone()); },
+        Expr::Literal(literal)    => { literals.entry(literal.clone()).or_insert(Word(count)); count + 1 },
         Expr::Token(_)
-        | Expr::Rule(_)           => (),
-        Expr::Seq(named_exprs, _) => { for named_expr in named_exprs { collect_literals(&named_expr.expr, literals) } }
-        Expr::Alt(exprs)          => { for expr in exprs { collect_literals(expr, literals) } },
+        | Expr::Rule(_)           => count,
+        Expr::Seq(named_exprs, _) => named_exprs.iter().fold(count, |acc, named_expr| collect_literals(&named_expr.expr, literals, acc)),
+        Expr::Alt(exprs)          => exprs.iter().fold(count, |acc, expr| collect_literals(expr, literals, acc)),
         Expr::Opt(expr)
         | Expr::Star(expr)
-        | Expr::Plus(expr)        => { collect_literals(expr, literals) },
+        | Expr::Plus(expr)        => collect_literals(expr, literals, count),
     }
 }
